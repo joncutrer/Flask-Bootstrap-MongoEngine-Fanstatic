d409679da7ab698c571a9afdadd9e85f9e7eeb5c
JC <joncutrer@gmail.com>
2017-03-20 20:57:11 -0500

    Update admin.py

--- webapp/admin.py
+++ webapp/admin.py
@@ -7,9 +7,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from flask.ext.superadmin import Admin, AdminIndexView
-from flask.ext.superadmin.model.backends.mongoengine import ModelAdmin
-from flask.ext.login import current_user
+from flask_superadmin import Admin, AdminIndexView
+from flask_superadmin.model.backends.mongoengine import ModelAdmin
+from flask_login import current_user
 
 from webapp import app
 from .models import User, DummyContent
